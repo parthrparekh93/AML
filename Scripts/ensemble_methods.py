8aa83deca1b628819a175605bdb1952d412aaa92
Rohan Kulkarni <rohan@dyn-160-39-166-35.dyn.columbia.edu>
2016-04-21 20:45:11 -0400

    ensemble done

--- Scripts/ensemble_methods.py
+++ Scripts/ensemble_methods.py
@@ -5,19 +5,48 @@ Created on Thu Apr 21 18:27:48 2016
 @author: rohan
 """
 
-from sklearn.cross_validation import cross_val_score
-from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
 from xgboost.sklearn import XGBClassifier
+import pandas as pd
 
-
-def main():
-    iris = load_iris()
-    clf = AdaBoostClassifier(n_estimators=100)
-    print clf
-    scores = cross_val_score(clf, iris.data, iris.target)
-    print scores.mean()
+def adaboost_algorithm(XTrain,YTrain,XTest):
+    adb = AdaBoostClassifier(n_estimators=100)
+    adb.fit(XTrain, YTrain)
+    y_pred_adaboost = adb.predict_proba(XTest)
+    return y_pred_adaboost
+    
+def xgboost_algorithm(XTrain,YTrain,XTest):
+    xgb = XGBClassifier(max_depth=6, learning_rate=0.3, n_estimators=25,
+                    objective='multi:softprob', subsample=0.5, colsample_bytree=0.5, seed=0)                  
+    xgb.fit(XTrain, YTrain)
+    y_pred_xgboost = xgb.predict_proba(XTest) 
+    return y_pred_xgboost
+    
+def ensemble_methods(algorithm='xgboost'):
+    XTrain = pd.read_csv('training_data_processed.csv',delimiter=',',encoding='utf-8')
+    XTest= pd.read_csv('testing_data_processed.csv',delimiter=',',encoding='utf-8')  
+    YTrain = XTrain['country_destination']
+    del XTrain['country_destination']
+    
+    XTrain=XTrain.drop(XTrain.columns[[0]], axis=1)
+    XTest=XTest.drop(XTest.columns[[0]], axis=1)
+    
+    XTest['language'].fillna(XTrain['language'].median(), inplace="True")
+    XTest['first_browser'].fillna(XTest['first_browser'].median(), inplace="True")
+  
+    if algorithm == 'adaboost':
+        print 
+        print 'Training AdaBoost Classifier...'
+        prediction_labels = adaboost_algorithm(XTrain,YTrain,XTest)
+    
+    elif algorithm == 'xgboost':
+        print 
+        print 'Training XGBoost Classifier...'
+        prediction_labels = xgboost_algorithm(XTrain,YTrain,XTest)
+    
+    ranked_results=[[row.argsort()[-5:][::-1]] for row in prediction_labels]
+    print ranked_results[:3]
     
     
 if __name__=='__main__':
-    main()
+    ensemble_methods(algorithm='xgboost')
